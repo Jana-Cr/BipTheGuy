9e66973cdace15bdf70f81eea5118e31f312558f
Janai Cruz <jancr2025@gmail.com>
2026-04-08 10:57:33 -0700

    started bip the guy: added a touch sound so whenever the image is touched, it creates a punching sound. Used animations to creat this spring as if the clown image is actually being punched

--- BipTheGuy/ContentView.swift
+++ BipTheGuy/ContentView.swift
@@ -6,16 +6,56 @@
 //
 
 import SwiftUI
+import AVFAudio
 
 struct ContentView: View {
+    @State private var audioPlayer: AVAudioPlayer!
+    @State private var isFullSize = true
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            
+            Image("clown")
+                .resizable()
+                .scaledToFit()
+                .scaleEffect(isFullSize ? 1.0 : 0.9)
+                .onTapGesture {
+                    playSound(soundName: "punchSound")
+                    isFullSize = false //will shrink using .scale effect to 90% of size
+                    withAnimation (.spring(response: 0.3, dampingFraction: 0.3)) {
+                        isFullSize = true //will go from 90% to 100% size but using .spring animation
+                    }
+                }
+                
+            
+            Spacer()
+            
+            Button {
+             //Button action soon
+            } label: {
+                Label("Photo Library", systemImage: "photo.fill.on.rectangle.fill")
+            }
+            
+            
         }
         .padding()
+    }
+    func playSound(soundName: String) {
+        if audioPlayer != nil && audioPlayer.isPlaying {
+            audioPlayer.stop()
+        }
+        guard let soundFile = NSDataAsset(name: soundName) else {
+            print("🥀Could not read file named \(soundName)")
+            return
+        }
+        do {
+            audioPlayer = try AVAudioPlayer(data: soundFile.data)
+            audioPlayer.play()
+        }catch {
+            print("🥀 ERROR: \(error.localizedDescription) creating audioPlayer")
+        }
+        
     }
 }
 
